2ff879ce405d44e4c7a93a54b1d7ff5e6e57df4d
KisicM <mkisic99@gmail.com>
2022-12-29 10:54:50 +0100

    created blockchain structure and flask app

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,54 @@
-#Module 1 - Create a Blockchain
+import datetime, hashlib, json
+
+class Blockchain:
+    def __init__(self) -> None:
+        self.chain = []
+        self.create_block(proof = 1, prev_hash = '0')
+
+    def create_block(self, proof, prev_hash) -> dict:
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'prev_hash': prev_hash
+        }
+        self.chain.append(block)
+        return block
+    
+    def get_last_block(self) -> dict:
+        return self.chain[-1]
+
+    def proof_of_work(self, prev_proof) -> int:
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            nonce = self.get_nonce(prev_proof, new_proof)
+            if nonce[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+    def get_nonce(self, prev_proof, new_proof):
+        return hashlib.sha256(self.non_symmetric_operation(prev_proof, new_proof).encode()).hexdigest()
+    
+    def non_symmetric_operation(self, prev_proof, new_proof) -> str:
+        return str(new_proof**2 - prev_proof**2) #A^2-B^2 != B^2-A^2, while A+B = B+A
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self, chain) -> bool:
+        prev_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['prev_hash'] != self.hash(prev_block):
+                return False
+            nonce = self.get_nonce(prev_block['proof'], block['proof'])
+            if nonce[:4] != '0000':
+                return False
+            prev_block = block
+            block_index += 1
+        return True
---
+++ flask_app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify
+from blockchain import Blockchain
+
+#Create Web App
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+#Init blockchain
+blockchain = Blockchain()
+
+#Send request
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    prev_block = blockchain.get_last_block()
+    proof = blockchain.proof_of_work(prev_block['proof'])
+    prev_hash = blockchain.hash(prev_block)
+    new_block = blockchain.create_block(proof, prev_hash)
+    response = {
+        'message': 'Congratulations, you just mined a block!',
+        'index': new_block['index'],
+        'timestamp': new_block['timestamp'],
+        'proof': new_block['proof'],
+        'prev_hash': new_block['prev_hash']
+    }
+    return jsonify(response), 200
+
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+#run server
+app.run(host = '0.0.0.0', port = 5000)
